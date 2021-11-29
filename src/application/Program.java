74db695ca936485735ddef5ec2344884f3f9946a
MarlonSilva21 <josemarlondasilva48@gmail.com>
2021-11-29 12:53:42 -0300

    speciaÃl move - promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,10 +5,7 @@ import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 
-import java.util.ArrayList;
-import java.util.InputMismatchException;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Program {
 
@@ -41,6 +38,12 @@ public class Program {
 
                 if(capturedPiece != null) {
                     captured.add(capturedPiece);
+                }
+
+                if (chessMatch.getPromoted() != null) {
+                    System.out.print("Enter piece for promotion (B/R/N/Q): ");
+                    String type = sc.nextLine().toUpperCase();
+                    chessMatch.replacePromotedPiece(type);
                 }
             }
             catch (ChessException | InputMismatchException e){
